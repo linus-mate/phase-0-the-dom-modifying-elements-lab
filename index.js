d6441f15da97bfcd4bf1bb051a35886faaead08a
linus mutugi mate <mlynux@gmail.com>
2022-08-05 12:14:47 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code here!
+// remove DOM 'main' node 
+const main = document.getElementById("main");
+main.remove();
+
+//create newHeader variable pointing to 'h1'
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+
+//add the element as a child
+document.body.appendChild(newHeader);
+
+//add content to the node
+newHeader.innerHTML = "Linus is the champion";
